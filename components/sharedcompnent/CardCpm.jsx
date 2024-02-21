4925c2fb2659cf89fe06aae385c503019f054400
devnadish <devnadish@gmail.com>
2024-02-21 09:59:01 +0300

    v01

--- components/sharedcompnent/CardCpm.jsx
+++ components/sharedcompnent/CardCpm.jsx
@@ -3,15 +3,18 @@ import {
     Card,
     CardContent,
   } from "@/components/ui/card";
+import MotionDiv from './MotionDiv';
    
 
 function CardCpm({children,topBorderColor="border-primary",h="min-h-72",borderSize="border-t-4" ,minW="min-w-[300px]"}) {
   return (
+    <MotionDiv>
     <Card dir="RTL" className={`  overflow-hidden rounded-none border-none hover:bg-secondary/5`} >
       <CardContent className={`${minW} flex flex-col  justify-between     py-3 overflow-hidden  ${h} ${borderSize} ${topBorderColor}  `}>
         {children}
       </CardContent>
     </Card>
+    </MotionDiv>
   );
 }
 
---
+++ components/sharedcompnent/MotionDiv.jsx
@@ -0,0 +1,20 @@
+
+'use client'
+
+import { motion } from 'framer-motion'
+function MotionDiv({ children }) {
+  return (
+     
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ ease: 'easeInOut', duration: 1 }}
+          >
+            {children}
+          </motion.div>
+       
+     
+  )
+}
+
+export default MotionDiv
